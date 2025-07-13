eeecc616dfecd64d30ed6be65181f702cbe3a8ef
Carlos Adrian Hernandez Aguilar <adrianprog@yahoo.com.mx>
2025-07-13 17:47:05 -0600

    se agregaron, componentes, modulos y rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './web/inicio/inicio';
+import { Servicios } from './web/servicios/servicios';
+import { Nosotros } from './web/nosotros/nosotros';
+import { Contactos } from './web/contactos/contactos';
+import { Error404 } from './errors/error404/error404';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Inicio },
+    { path: 'servicios', component: Servicios },
+    { path: 'nosotros', component: Nosotros },
+    { path: 'contactos', component: Contactos },
+    { path: 'auth', loadChildren: () => import("./auth/auth-module").then(m => m.AuthModule) }, // Solo se habilita el modulo
+    { path: 'admin', loadChildren: () => import("./admin/admin-module").then(m => m.AdminModule) }, // Se habilita el modulo de administracion
+    { path: '**', component: Error404 }
+];
