3ee8f8d6fb53f70aebe5b03affea1472f6971f4f
Carlos Adrian Hernandez Aguilar <adrianprog@yahoo.com.mx>
2025-07-20 20:23:03 -0600

    add componentes

--- src/app/admin/admin-module.ts
+++ src/app/admin/admin-module.ts
@@ -2,18 +2,29 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing-module';
-import { Perfil } from './perfil/perfil';
-import { User } from './user/user';
-
+import { Perfil } from './components/perfil/perfil';
+import { User } from './components/user/user';
+import { Role } from './components/role/role';
+import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { TableModule } from 'primeng/table';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     Perfil,
-    User
+    User,
+    Role,
   ],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    ReactiveFormsModule,
+    DialogModule,
+    ButtonModule,
+    InputTextModule,
+    TableModule
   ]
 })
 export class AdminModule { }
---
+++ src/app/admin/components/role/role.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-role',
+  standalone: false,
+  templateUrl: './role.html',
+  styleUrl: './role.scss'
+})
+export class Role {
+
+}
